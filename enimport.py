f469358deaea07ab58e6394852af69091fb19ed3
Matthew Leingang <leingang@nyu.edu>
2014-09-22 11:09:15 -0400

    Added support for the document's date; removed errorful configuration
    You can now specify the document's date on the command line with the
    --docdate argument and in the config file with the doc_date variable.
    
    Disabled the default option values for docname and docdate because they
    clobbered the variables set in the config file.

--- enimport.py
+++ enimport.py
@@ -23,24 +23,51 @@ import logging
 import csv
 import sys
 import mimetypes
+import dateutil.parser          # sudo port -v install py27-dateutil
+from dateutil.tz import *
+from datetime import datetime, timedelta
+import time
 
-parser = argparse.ArgumentParser()
-# config file:
-# FIXME: this method clobbers all the help in the later options.
-# see http://stackoverflow.com/a/5826167/297797 for a fix
-# TODO: multiple config files (e.g., ./.enimportrc, ~/enimport.rc, explicit files)
-parser.add_argument("-c", "--config",
-                    help="Specify config file", metavar="FILE")
-args, remaining_argv = parser.parse_known_args()
-defaults={}
-if args.config:
-    config = ConfigParser.SafeConfigParser()
-    config.read([args.config])
-    # TODO: jury-rig config.parser so it doesn't need a dummy section
-    # http://stackoverflow.com/a/8657601/297797
-    defaults = dict(config.items("defaults"))
+def toTimestamp(dt, epoch=datetime.fromtimestamp(0,tzutc())):
+	""" 
+	convert a datetime object to a unix timestamp 
+
+	See http://stackoverflow.com/a/8778548/297797
+	"""
+	logging.debug("epoch: %s",repr(epoch))
+	td = dt - epoch
+	# return td.total_seconds()
+	return (td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6) / 1e6 
+
+
+# Parse any conf_file specification
+# We make this parser with add_help=False so that
+# it doesn't parse -h and print help.
+conf_parser = argparse.ArgumentParser(
+	description=__doc__, # printed with -h/--help
+	# Don't mess with format of description
+	formatter_class=argparse.RawDescriptionHelpFormatter,
+	# Turn off help, so we print all options in response to -h
+	add_help=False
+	)
+conf_parser.add_argument("-c", "--conf",
+					help="Specify config file (default: enimport.rc)", metavar="FILE",
+					default="enimport.rc")
+args, remaining_argv = conf_parser.parse_known_args()
+
+if args.conf:
+	config = ConfigParser.SafeConfigParser()
+	config.read([args.conf])
+	defaults = dict(config.items("defaults"))
+else:
+	defaults = { }
+# Parse rest of arguments
+# Don't suppress add_help here so it will handle -h
+parser = argparse.ArgumentParser(
+	# Inherit options from config_parser
+	parents=[conf_parser]
+	)
 parser.set_defaults(**defaults)
-    
 parser.add_argument('-d','--debug',
     help='Print lots of debugging statements',
     action="store_const",dest="loglevel",const=logging.DEBUG,
@@ -66,15 +93,19 @@ parser.add_argument('-nb','--notebook',
 	)
 parser.add_argument('--docname',
 	help="Name of the document",
-	default="Untitled document",
+	# default="Untitled document",
 	action="store", dest="doc_name")
+parser.add_argument('--docdate',
+	help="Date of the document (use ISO 8601 format)",
+	action="store",dest='doc_date')
 parser.add_argument('--course',
 	help="Name of the course",
 	action="store", dest="course")
 parser.add_argument('--term',
 	help='term name',
 	action='store',dest='term',
-	default="Fall 2014")
+	# default="Fall 2014" FIXME: defaults here clobber values from config file :-(
+	)
 # TODO: tag names (multiple optional argument)
 parser.add_argument('--tag',metavar='TAG',
 	help='Add tag to note (as many as you like)',
@@ -155,7 +186,19 @@ for rec in csv.reader(args.csvfile):
 	logging.info("note.tags: %s",repr(note.tagNames))
 	## TODO: add some more note attributes
 	# created - exam date/time (Timestamp) # parse ISO 8601!
+	currentTime = time.time() * 1000
+	if (args.doc_date):
+		createdDate = dateutil.parser.parse(args.doc_date)
+		logging.debug("createdDate: %s",repr(createdDate))
+		createdTimestamp = toTimestamp(createdDate)
+		note.created =  createdTimestamp * 1000
+		logging.info("note.created: %d",note.created)
+	else:
+		note.created=currentTime
+		logging.info("note.created: %d (now)",note.created)
 	# updated - now, obvs (Timestamp)
+	note.updated=currentTime
+	logging.info("note.updated: %d (now)",note.updated)
 	## TODO: add some more attributes with the NoteAttributes type
 	# https://dev.evernote.com/doc/reference/Types.html#Struct_NoteAttributes
 	# latitude
